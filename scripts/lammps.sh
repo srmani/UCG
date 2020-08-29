cea8f094ac9c93ef64b1f3abb2c0b87a9deba6d7
Sriramvignesh Mani <m.sriramvignesh@gmail.com>
2020-08-28 23:27:59 -0500

    modified .travis.yml

---
+++ scripts/lammps.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git clone -b stable https://github.com/lammps/lammps.git
+cd lammps
+cd src
+cp ../../src/fix_ucg_gfrd.cpp .
+cp ../../src/fix_ucg_gfrd.h .
+make yes-MOLECULE
+make yes-MC
+make yes-RIGID
+make mpi
+cd ../../
