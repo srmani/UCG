180ec6614d7a69f80e9ddbca7ebbd733ff0a73d6
Sriramvignesh Mani <m.sriramvignesh@gmail.com>
2020-09-13 16:14:27 -0500

    changed lammps to mdi-lammps

--- scripts/lammps.sh
+++ scripts/lammps.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-git clone -b stable https://github.com/lammps/lammps.git
+git clone -b mdi https://github.com/MolSSI-MDI/lammps.git
 cd lammps
 cd src
 cp ../../src/fix_ucg_gfrd.cpp .
 cp ../../src/fix_ucg_gfrd.h .
+make lib-mdi args="-m gcc"
+make yes-user-mdi
 make yes-MOLECULE
 make yes-MC
 make yes-RIGID
+make yes-kspace
 make mpi
 cd ../../
 pwd
